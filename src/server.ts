c13e1f92a78a41021a7a84926c29d02376053043
Victor Hugo <victorhugoos@live.com>
2022-07-03 21:41:03 -0300

    included process.env.PORT

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
 import { serverHttp } from "./app";
-const port = 4000
+const port = process.env.PORT || 4000
 
 serverHttp.listen(port, () => console.log(`\nHTTP Server running on port http://localhost:${port}/`))
